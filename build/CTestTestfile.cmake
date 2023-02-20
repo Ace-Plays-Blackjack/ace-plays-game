6757c92f4fceb189b9137f57085e963c42444393
Titas, Georgios <giorgostitas@gmail.com>
2023-02-20 23:50:18 +0000

    Add CMakeLists file and VideoCapture sample code from OpenCV

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/YEAR_5/Real_Time_Emb_Programming/Blackjack_files/ace-plays-game
+# Build directory: D:/YEAR_5/Real_Time_Emb_Programming/Blackjack_files/ace-plays-game/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
