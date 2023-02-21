f5261988c6757097276a4775955b36de56d41e0a
Titas, Georgios <giorgostitas@gmail.com>
2023-02-21 00:53:12 +0000

    Delete build files
    Keep build folder empty.

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/YEAR_5/Real_Time_Emb_Programming/Blackjack_files/ace-plays-game
-# Build directory: D:/YEAR_5/Real_Time_Emb_Programming/Blackjack_files/ace-plays-game/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
