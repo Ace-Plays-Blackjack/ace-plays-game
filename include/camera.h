7332f91e1f5445da6ac3d4416f92c913e771f7f3
Titas, Georgios <giorgostitas@gmail.com>
2023-02-28 12:14:54 +0000

    Add start/stop methods in camera class

--- include/camera.h
+++ include/camera.h
@@ -1,15 +1,28 @@
 #ifndef CAMERA_H
 #define CAMERA_H
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+
+#include <iostream>
+
+enum Err_type{
+    NO_ERROR,
+    ERR_INIT
+};
 
 class Camera
 {
 private:
-    /* data */
+    int err_code;
+    cv::Mat current_frame;
+    
 public:
+    cv::VideoCapture active_capture;
     Camera(/* args */);
     ~Camera();
-
-    void get_video_feed();
+    void start_recording();
+    void stop();
+    
 };
 
 
