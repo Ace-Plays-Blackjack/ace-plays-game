b5a016a1eb4c3bdfda7c2c2e30d90b8aef0bfd56
Eagleeye101 <123183921+Eagleeye101@users.noreply.github.com>
2023-03-01 12:24:49 +0000

    LED Implementation
    Started LED implementation

---
+++ include/leds.h
@@ -0,0 +1,7 @@
+class led_flasher {
+    public: 
+    led_flasher();
+    bool changeled(bool on);
+    bool mainthread();
+
+};
---
+++ src/leds.cpp
@@ -0,0 +1,13 @@
+#include <leds.h>
+
+
+led_flasher::led_flasher(){
+
+};
+bool led_flasher::changeled(bool on){
+
+};
+bool led_flasher::mainthread(){
+
+};
+
