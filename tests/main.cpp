acc03a4773124a649ec2098719dcf5babe4f9d17
jijoNV <jijon92@gmail.com>
2023-04-16 16:54:12 +0100

    Added header

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,8 +1,11 @@
 #include <gtest/gtest.h>
+#include "Card.h"
+#include "Camera.h"
+#include "CardDiscriminator.h"
+#include "DetectCard.h"
+#include "GamePlay.h"
 #include "Leds.h"
 #include "StrategyEngine.h"
-#include "camera.h"
-
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
